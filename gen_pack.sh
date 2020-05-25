b0bb3f4e675aa560c00dfeb6e0e9aa280753f0db
Robert Rostohar <Robert.Rostohar@arm.com>
2020-05-25 11:30:58 +0200

    Pack: Update build script (V1.1)

--- gen_pack.sh
+++ gen_pack.sh
@@ -1,36 +1,55 @@
 #!/bin/bash
-# Version: 1.0 
-# Date: 2019-08-16
+# Version: 1.1
+# Date: 2020-05-14
 # This bash script generates a CMSIS Software Pack:
 #
 # Pre-requisites:
 # - bash shell (for Windows: install git for Windows)
 # - 7z in path (zip archiving utility)
-#   e.g. Ubuntu: sudo apt-get install p7zip-full p7zip-rar) 
-# - PackChk in path with execute permission
-#   (see CMSIS-Pack: CMSIS/Utilities/<os>/PackChk)
+#   e.g. Ubuntu: sudo apt-get install p7zip-full p7zip-rar)
+# - PackChk is taken from latest install CMSIS Pack installed in $CMSIS_PACK_ROOT
 # - xmllint in path (XML schema validation; available only for Linux)
 
 ############### EDIT BELOW ###############
 # Extend Path environment variable locally
 #
-if [ `uname -s` = "Linux" ]
-  then
-  CMSIS_PACK_PATH="/home/$USER/.arm/Packs/ARM/CMSIS/5.7.0/"
-  PATH_TO_ADD="$CMSIS_PACK_PATH/CMSIS/Utilities/Linux64/"
-else
-  CMSIS_PACK_PATH="/C/ARM/PACK/ARM/CMSIS/5.7.0"
-  PATH_TO_ADD="/C/Program Files/7-Zip/:$CMSIS_PACK_PATH/CMSIS/Utilities/Win32/"
-fi
+
+OS=$(uname -s)
+case $OS in
+  'Linux')
+    if [ -z ${CMSIS_PACK_ROOT+x} ] ; then
+      CMSIS_PACK_ROOT="/home/$USER/.arm/Packs"
+    fi
+    CMSIS_TOOLSDIR="$(ls -drv ${CMSIS_PACK_ROOT}/ARM/CMSIS/* | head -1)/CMSIS/Utilities/Linux64"
+    ;;
+  'WindowsNT'|MINGW*|CYGWIN*)
+    if [ -z ${CMSIS_PACK_ROOT+x} ] ; then
+      CMSIS_PACK_ROOT="$LOCALAPPDATA/Arm/Packs"
+    fi
+    CMSIS_PACK_ROOT="/$(echo ${CMSIS_PACK_ROOT} | sed -e 's/\\/\//g' -e 's/://g' -e 's/\"//g')"
+    CMSIS_TOOLSDIR="$(ls -drv ${CMSIS_PACK_ROOT}/ARM/CMSIS/* | head -1)/CMSIS/Utilities/Win32"
+    ;;
+  'Darwin') 
+    echo "Error: CMSIS Tools not available for Mac at present."
+    exit 1
+    ;;
+  *)
+    echo "Error: unrecognized OS $OS"
+    exit 1
+    ;;
+esac
+
+PATH_TO_ADD="$CMSIS_TOOLSDIR"
+
 [[ ":$PATH:" != *":$PATH_TO_ADD}:"* ]] && PATH="${PATH}:${PATH_TO_ADD}"
 echo $PATH_TO_ADD appended to PATH
 echo " "
 
 # Pack warehouse directory - destination 
-PACK_WAREHOUSE=output/
+PACK_WAREHOUSE=./output
 
 # Temporary pack build directory
-PACK_BUILD=build/
+PACK_BUILD=./build
 
 # Specify directories included in pack relative to base directory
 # All directories:
@@ -59,7 +78,7 @@ PACK_BASE_FILES="
 echo Starting CMSIS-Pack Generation: `date`
 # Zip utility check 
 ZIP=7z
-type -a $ZIP
+type -a "${ZIP}"
 errorlevel=$?
 if [ $errorlevel -gt 0 ]
   then
@@ -71,14 +90,14 @@ fi
 
 # Pack checking utility check
 PACKCHK=PackChk
-type -a $PACKCHK
+type -a ${PACKCHK}
 errorlevel=$?
 if [ $errorlevel != 0 ]
   then
   echo "Error: No PackChk Utility found"
   echo "Action: Add PackChk to your path"
   echo "Hint: Included in CMSIS Pack:"
-  echo "<pack_root_dir>/ARM/CMSIS/<version>/CMSIS/Utilities/<os>/"
+  echo "$CMSIS_PACK_ROOT/ARM/CMSIS/<version>/CMSIS/Utilities/<os>/"
   echo " "
   exit
 fi
@@ -88,11 +107,11 @@ echo " "
 # check whether there is more than one pdsc file
 NUM_PDSCS=`ls -1 *.pdsc | wc -l`
 PACK_DESCRIPTION_FILE=`ls *.pdsc`
-if [ $NUM_PDSCS -lt 1 ]
+if [ ${NUM_PDSCS} -lt 1 ]
   then
   echo "Error: No *.pdsc file found in current directory"
   echo " "
-elif [ $NUM_PDSCS -gt 1 ]
+elif [ ${NUM_PDSCS} -gt 1 ]
   then
   echo "Error: Only one PDSC file allowed in directory structure:"
   echo "Found:"
@@ -104,49 +123,51 @@ fi
 
 SAVEIFS=$IFS
 IFS=.
-set $PACK_DESCRIPTION_FILE
+set ${PACK_DESCRIPTION_FILE}
 # Pack Vendor
 PACK_VENDOR=$1
 # Pack Name
 PACK_NAME=$2
-echo Generating Pack Version: for $PACK_VENDOR.$PACK_NAME
+echo "Generating Pack Version: for $PACK_VENDOR.$PACK_NAME"
 echo " "
 IFS=$SAVEIFS
 
 #if $PACK_BUILD directory does not exist, create it.
-if [ ! -d $PACK_BUILD ]; then
-  mkdir -p $PACK_BUILD
+if [ ! -d "$PACK_BUILD" ]; then
+  mkdir -p "$PACK_BUILD"
 fi
 
 # Copy files into build base directory: $PACK_BUILD
 # pdsc file is mandatory in base directory:
-cp -f  ./$PACK_VENDOR.$PACK_NAME.pdsc ${PACK_BUILD}
+cp -f  "./${PACK_VENDOR}.${PACK_NAME}.pdsc" "${PACK_BUILD}"
 
 # directories
 echo Adding directories to pack:
-echo $PACK_DIRS
+echo "${PACK_DIRS}"
 echo " "
 for d in ${PACK_DIRS}
 do
-  cp -r "$d" ${PACK_BUILD}
+  cp -r "$d" "${PACK_BUILD}"
 done
 
 # files for base directory
 echo Adding files to pack:
-echo $PACK_BASE_FILES
+echo "${PACK_BASE_FILES}"
 echo " "
-for f in $PACK_BASE_FILES
-do 
-  cp -f  "$f" $PACK_BUILD/ 
-done
+if [ ! -x ${PACK_BASE_FILES+x} ]; then
+  for f in ${PACK_BASE_FILES}
+    do
+      cp -f  "$f" $PACK_BUILD/ 
+  done
+fi
 
 # Run Schema Check (for Linux only):
 # sudo apt-get install libxml2-utils
 
-if [ `uname -s` = "Linux" ]
+if [ $(uname -s) = "Linux" ]
   then
-  echo Running schema check for $PACK_VENDOR.$PACK_NAME.pdsc
-  xmllint --noout --schema ${CMSIS_PACK_PATH}/CMSIS/Utilities/PACK.xsd $PACK_BUILD/$PACK_VENDOR.$PACK_NAME.pdsc
+  echo "Running schema check for ${PACK_VENDOR}.${PACK_NAME}.pdsc"
+  xmllint --noout --schema "${CMSIS_TOOLSDIR}/../PACK.xsd" "${PACK_BUILD}/${PACK_VENDOR}.${PACK_NAME}.pdsc"
   errorlevel=$?
   if [ $errorlevel -ne 0 ]; then
     echo "build aborted: Schema check of $PACK_VENDOR.$PACK_NAME.pdsc against PACK.xsd failed"
@@ -158,7 +179,7 @@ else
 fi
 
 # Run Pack Check and generate PackName file with version
-$PACKCHK $PACK_BUILD/$PACK_VENDOR.$PACK_NAME.pdsc -n PackName.txt -x M362
+"${PACKCHK}" "${PACK_BUILD}/${PACK_VENDOR}.${PACK_NAME}.pdsc" -n PackName.txt -x M362
 errorlevel=$?
 if [ $errorlevel -ne 0 ]; then
   echo "build aborted: pack check failed"
@@ -166,21 +187,22 @@ if [ $errorlevel -ne 0 ]; then
   exit
 fi
 
-PACKNAME=`cat PackName.txt`
+PACKNAME=$(cat PackName.txt)
 rm -rf PackName.txt
 
 # Archiving
 # $ZIP a $PACKNAME
-echo creating pack file $PACKNAME
+echo "creating pack file $PACKNAME"
 #if $PACK_WAREHOUSE directory does not exist create it
-if [ ! -d $PACK_WAREHOUSE ]; then
-  mkdir -p $PACK_WAREHOUSE
+if [ ! -d "$PACK_WAREHOUSE" ]; then
+  mkdir -p "$PACK_WAREHOUSE"
 fi
-pushd $PACK_WAREHOUSE
-PACK_WAREHOUSE=`pwd`
+pushd "$PACK_WAREHOUSE"
+PACK_WAREHOUSE=$(pwd)
 popd
-pushd $PACK_BUILD
-"$ZIP" a $PACK_WAREHOUSE/$PACKNAME -tzip
+pushd "$PACK_BUILD"
+PACK_BUILD=$(pwd)
+"$ZIP" a "$PACK_WAREHOUSE/$PACKNAME" -tzip
 popd
 errorlevel=$?
 if [ $errorlevel -ne 0 ]; then
@@ -192,7 +214,7 @@ echo "build of pack succeeded"
 # Clean up
 echo "cleaning up ..."
 
-rm -rf $PACK_BUILD
+rm -rf "$PACK_BUILD"
 echo " "
 
-echo Completed CMSIS-Pack Generation: `date`
+echo Completed CMSIS-Pack Generation: $(date)
